f24d521c6991b177ccc87fc4bda82154f728d018
Mathis Durand <mathis.durand@hotmail.com>
2026-07-27 11:38:44 +0200

    first step done with everything saved in the npz file

--- src/dataset.py
+++ src/dataset.py
@@ -1,14 +1,15 @@
 from features import build_dataset
 from datetime import date
-
+import numpy as np
+from pathlib import Path
 
 
 start_date = date(2026, 3, 1)
 end_date = date.today()
 final, all_dates = build_dataset(start_date, end_date, "data/garmin_data.db")
 
-# train/test split
-cutoff = int(0.85*len(all_dates))
+# Train/test split
+cutoff = int(0.85*len(all_dates)) # 0.85 is arbitrary
 
 final_train = final[:cutoff]
 dates_train = all_dates[:cutoff]
@@ -16,11 +17,19 @@ dates_train = all_dates[:cutoff]
 final_val = final[cutoff:]
 dates_val = all_dates[cutoff:]
 
-print(final_train.shape)
-print(len(dates_train))
+# Normalization
+mean = final_train.mean(axis=(0, 1)) # I "deleted" 2 dimensions (axis=(0,1)) at once in order to get the mean for each column
+std = final_train.std(axis=(0, 1))
 
-for l in [final_train, dates_train, final_val, dates_val]:
-    print(f"First value: {l[0]}")
-    print(f"Last value: {l[-1]}")
+final_train_norm = (final_train - mean) / std
+final_val_norm = (final_val - mean) / std
 
+dates_train_array = np.array(dates_train) # In order to save it in the npz file
+dates_val_array = np.array(dates_val)
 
+ARRAYS_PATH = Path("data")
+np.savez(file = ARRAYS_PATH / "dataset_normalized.npz", train = final_train_norm, val = final_val_norm,
+         dates_train = dates_train_array, dates_val = dates_val_array, mean = mean, std = std)
+
+test = np.load(ARRAYS_PATH / "dataset_normalized.npz")
+print(test["train"].shape)
